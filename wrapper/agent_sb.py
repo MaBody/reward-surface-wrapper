4dbf5684511b214a08a9be1828f3fb5af0dd6817
Mattis Bodynek <mattisbodynek@gmail.com>
2025-07-10 13:20:07 +0200

    started implementing sb3 wrapper

--- wrapper/agent_sb.py
+++ wrapper/agent_sb.py
@@ -1,17 +1,22 @@
 from abc import ABC
 from numpy.typing import ArrayLike
 from wrapper.agent import AgentWrapper
+from stable_baselines3.common.base_class import BaseAlgorithm
 
 
 class StableBaselinesWrapper(AgentWrapper):
-    @classmethod
-    def initialize(cls, weights: list[ArrayLike]) -> AgentWrapper:
+
+    def __init__(self, agent: BaseAlgorithm):
+        self.agent = agent
+
+    # @classmethod
+    def initialize(self, weights: list[ArrayLike]) -> AgentWrapper:
         # makes new agent with provided weights
-        pass
+        return self.agent.policy.load_from_vector(weights)
 
     def get_weights(self) -> list[ArrayLike]:
         # returns weights as a list of (torch|numpy) arrays
-        pass
+        return self.agent.policy.parameters_to_vector()
 
     def estimate_loss(self, n_steps: int, **kwargs) -> float:
         # evaluates policy for n_steps and returns loss estimate
